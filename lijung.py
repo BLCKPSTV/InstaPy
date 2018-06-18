9e096e5616c8a17716f893b1db8a82eadb676652
BLCKPSTV <dewildequinten@gmail.com>
2018-06-18 23:30:58 +0200

    updated lijung

--- lijung.py
+++ lijung.py
@@ -7,8 +7,8 @@ from selenium.common.exceptions import NoSuchElementException
 
 from instapy import InstaPy
 
-insta_username = '_kaorikondo_'
-insta_password = 'zv!~8y?$7N7^'
+insta_username = 'lijung84'
+insta_password = '_%ot5ZPe'
 
 # set headless_browser=True if you want to run InstaPy on a server
 
@@ -29,7 +29,7 @@ def job():
 		# default enabled=False , enables the checking with the clarifai api (image tagging)
 		# if secret and proj_id are not set, it will get the environment Variables
 		# 'CLARIFAI_API_KEY'
-		session.set_use_clarifai(enabled=True, api_key='f56aa5af66994d4f92eaf5e8e7db8e03')
+		session.set_use_clarifai(enabled=True, api_key='f58bf3c3d55e46929c4ffdc83da8864a')
 		session.set_use_clarifai(enabled=True)
    	 	session.set_relationship_bounds(enabled=True,
 						 potency_ratio=-1.21,
@@ -39,23 +39,22 @@ def job():
 				     		min_followers=45,
 				      		min_following=77)
    	 	session.set_do_comment(True, percentage=10)
-    		session.set_comments([u'Nice shot! :thumbsup:', 'Where is that?', 'Need to do that too!'], media='Photo')
-    		session.set_comments(['Great Video!', u'How artful :sparkles:'], media='Video')
+    		session.set_comments([u'Interesting stuff! :thumbsup:', 'where did you get the inspiration from?', 'Nice photograph!'], media='Photo')
+    		session.set_comments(['Great Video, nicely made!', u'very professional! :sparkles:'], media='Video')
     		session.set_comments(['Nice shot! @{}', 'Couldnt have done it any better! @{}'], media='Photo')
-    		session.set_comments(['Cool!', 'Awesome!', 'Nice!', 'Super!', u'Sugoi! :trollface:', u':raised_hands: Automation Collective (Social Flux) :raised_hands:'])
+    		session.set_comments(['Cool!', 'Awesome!', '喜欢它', 'Super!', u'冷静 :trollface:', u':raised_hands: Automation Collective (Social Flux) :raised_hands:'])
    		session.set_dont_include(['random_user'])
-    		session.set_dont_like(['pizza', 'girl', 'raciscme', 'pro trump'])
+    		session.set_dont_like([ 'girl', 'raciscme', 'pro trump'])
 		session.set_user_interact(amount=5, random=True, percentage=50, media='Photo')
 
 		 # session clarifai actions
-		session.clarifai_check_img_for(['food'], comment=True, comments=['Tasty!', 'Yum!'])
-		session.clarifai_check_img_for(['ceramic'], comment=True, comments=['Nice Design', 'Cool color!'])
+		session.clarifai_check_img_for(['writing'], comment=True, comments=['Beautiful!', 'Inspiring!'])
 
    		 # actions
-    		session.like_by_tags(['designer', 'webdesign', 'UX', 'design', 'ceramics', 'architecture', 'design art' ], amount=50)
+    		session.like_by_tags(['writing', 'poetry', 'writing artist', 'romance', 'shakespeare', 'russian writers', 'literature' ], amount=50)
 		session.like_by_users(usernames=['random_user'], amount=10, random=True)
 		session.like_by_feed(amount=10, randomize=True)
-		session.interact_user_followers(['natgeo', 'charity', 'design'], amount=10, random=True)
+		session.interact_user_followers(['writing', 'writers', 'markov'], amount=10, random=True)
 		session.unfollow_users(amount=2)
 		
 
@@ -75,10 +74,10 @@ def job():
     	 	session.end()
 	
 schedule.every().day.at("6:35").do(job)
-schedule.every().day.at("16:22").do(job)
-schedule.every().day.at("18:42").do(job)
+schedule.every().day.at("10:22").do(job)
+schedule.every().day.at("13:50").do(job)
 schedule.every().day.at("20:22").do(job)
-schedule.every().day.at("22:22").do(job)
+schedule.every().day.at("23:32").do(job)
 schedule.every().day.at("00:22").do(job)
 schedule.every().day.at("04:22").do(job)
 
